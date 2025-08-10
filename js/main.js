69c6965bfd5e09e0494b9e8fb05bd4252e0e1c65
arisa.W <eto.po.adress.ko@gmail.com>
2025-08-10 15:15:21 +0900

    時間調整

--- js/main.js
+++ js/main.js
@@ -32,9 +32,9 @@ $(function () {
     } catch (e) {}
 
     // ==== 時間設定 ====
-    const MSG_AT_MS = 5000;
-    const SECOND_MSG_DELAY = 2000;
-    const STAY_AFTER_SECOND_MS = 5000;
+    const MSG_AT_MS = 7000;
+    const SECOND_MSG_DELAY = 5000;
+    const STAY_AFTER_SECOND_MS = 7000;
 
     let started = false;
 
